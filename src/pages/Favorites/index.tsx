97500ad9bbe03101fdbba352547226c0a789655f
Luis Felipe <felipedelta0@gmail.com>
2020-08-09 23:12:20 -0300

    criado o teacher item

--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -1,13 +1,38 @@
 import React from 'react'
 import { View } from 'react-native'
+import { ScrollView } from 'react-native-gesture-handler'
+
+import TeacherItem from '../../components/TeacherItem'
+import PageHeader from '../../components/PageHeader'
 
 import styles from './styles'
-import PageHeader from '../../components/PageHeader'
 
 function Favorites() {
   return (
     <View style={styles.container}>
       <PageHeader title='Meus proffys favoritos' />
+
+      <ScrollView
+        style={styles.teacherList}
+        contentContainerStyle={{
+          paddingHorizontal: 16,
+          paddingBottom: 16
+        }}
+      >
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+      </ScrollView>
     </View>
   )
 }
--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,13 +1,39 @@
 import React from 'react'
 import { View } from 'react-native'
+import { ScrollView } from 'react-native-gesture-handler'
+
+import TeacherItem from '../../components/TeacherItem'
+import PageHeader from '../../components/PageHeader'
 
 import styles from './styles'
-import PageHeader from '../../components/PageHeader'
+
 
 function TeacherList() {
   return (
     <View style={styles.container}>
       <PageHeader title='Proffys diponíveis' />
+
+      <ScrollView
+        style={styles.teacherList}
+        contentContainerStyle={{
+          paddingHorizontal: 16,
+          paddingBottom: 16
+        }}
+      >
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+      </ScrollView>
     </View>
   )
 }
